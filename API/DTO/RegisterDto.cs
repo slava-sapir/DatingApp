3f27df477378cad204b2a06729a4428cfd0e5d1c
slava sapir <slavas21@gmail.com>
2021-01-10 18:06:39 -0500

    End of section 8

--- API/DTO/RegisterDto.cs
+++ API/DTO/RegisterDto.cs
@@ -6,6 +6,7 @@ namespace API.DTO
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
